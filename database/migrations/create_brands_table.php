aca34f84c8c2727fb53a188de8eabe012b41a5ce
Ezequiel Herrera <ezequiel.herrera@gmail.com>
2017-12-11 05:44:58 +0100

    First commit. Backup all files

---
+++ database/migrations/create_brands_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBrandsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('brands', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('logo_large_off');
+            $table->string('logo_large_on');
+            $table->string('logo_small_off');
+            $table->string('logo_small_on');
+            $table->string('catalogs');
+            $table->string('pricelists');
+            $table->string('web');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('brands');
+    }
+}
